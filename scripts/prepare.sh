62e79481e15f0b5a3b102456b59360bcfaa524d1
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-11-23 22:59:03 +0900

    add script

---
+++ scripts/prepare.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -ex
+
+# [TODO: 最初に設定] 環境変数
+GIT_REPOSITORY_DIR="$HOME/webapp"
+SYSTEMCTL_APP="isu-ruby"
+MYSQL_SLOW_LOG="/var/log/mysql/mysql-slow.log"
+NGINX_ACCESS_LOG="/var/log/nginx/access.log"
+APP_PROF="$GIT_REPOSITORY_DIR/ruby/tmp"
+
+git pull
+
+cp -r ${GIT_REPOSITORY_DIR}/etc/nginx/* /etc/nginx
+cp -r ${GIT_REPOSITORY_DIR}/etc/mysql/* /etc/mysql
+
+sudo truncate -s 0 "${MYSQL_SLOW_LOG}"
+sudo truncate -s 0 "${NGINX_ACCESS_LOG}"
+if [ -d $APP_PROF ]; then
+  rm -r ${APP_PROF}
+fi
+
+sudo systemctl restart ${SYSTEMCTL_APP}
+sudo systemctl restart nginx
+sudo systemctl restart mysql
---
+++ scripts/result.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# [TODO: 最初に設定] 環境変数
+APP_HOME=~/private_isu/webapp
+MYSQL_SLOW_LOG="/var/log/mysql/slow.log"
+NGINX_ACCESS_LOG="/var/log/nginx/access.log"
+NGINX_ACCESS_LOG_FORMAT="${APP_HOME}/measure/nginx/access.log"
+ALPSORT=sum
+
+# [TODO: API見ながら設定] パスパターンを配列として定義
+ALP_PATTERNS=(
+    "^/login$"
+    "^/comment$"
+    "^/posts/\d+$"
+    "^/posts\?max_created_at=.*$"
+    "^/image/\d+\.(png|jpg)$"
+    "^/@[^/]+$"
+    "^/admin/banned$"
+    "^/js/main\.js$"
+    "^/js/timeago\.min\.js$"
+    "^/favicon\.ico$"
+    "^/$"
+)
+
+ALPM=$(IFS=,; echo "${ALP_PATTERNS[*]}")
+OUTFORMAT=count,method,uri,min,max,sum,avg,p99
+
+# alp
+sudo alp ltsv --file=${NGINX_ACCESS_LOG} --nosave-pos --pos /tmp/alp.pos --sort ${ALPSORT} --reverse -o ${OUTFORMAT} -q -m ${ALPM} > ${NGINX_ACCESS_LOG_FORMAT}
+echo "Nginxのログフォーマット完了"
+
+# mysql
+sudo mysqldumpslow -s t /var/log/mysql/mysql-slow.log > ${APP_HOME}/measure/mysql/mysql-slow.log
+echo "Mysqlのスロークエリ出力完了"
+
+# ruby
+stackprof ${APP_HOME}/ruby/tmp/stackprof-* > ${APP_HOME}/measure/ruby/stackprof.txt
+cat ${APP_HOME}/measure/ruby/stackprof.txt
+echo "stackprofのtext出力完了"
+if [ -d ${APP_HOME}/ruby/tmp ]; then
+  rm -r ${APP_HOME}/ruby/tmp
+fi
